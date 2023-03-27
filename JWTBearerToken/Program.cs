2fda9f6f2beb2886096a535f91fee2987c914a77
Laurence Harker <laurence.harker@288group.com>
2023-03-27 12:04:51 +0100

    #Initial commit

---
+++ JWTBearerToken/Program.cs
@@ -0,0 +1,14 @@
+﻿using Firebase.Auth;
+
+var username = "firebase user";
+var password = "firebase password";
+var apiKey = "firebase api key";
+
+var config = new FirebaseConfig(apiKey);
+
+var authProvider = new FirebaseAuthProvider(config);
+var result = await authProvider.SignInWithEmailAndPasswordAsync(username, password);
+var token = result.FirebaseToken;
+
+Console.WriteLine("Bearer " + token);
+Console.WriteLine(" ");
